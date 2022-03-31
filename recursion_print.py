2e7892bdd9b15485d603c7db303b9217cacf98ca
IzbushkO <98118884+Izbushkoo@users.noreply.github.com>
2022-03-31 11:46:02 +0300

    Update recursion_print.py

--- recursion_print.py
+++ recursion_print.py
@@ -1,6 +1,5 @@
 #  Function prints numbers from "start" to "stop" without using loops.
 
-
 def print_num(start, stop):
     if start == stop:
         print(start)
@@ -12,4 +11,18 @@ def print_num(start, stop):
 stop = int(input("Enter stop number: "))
 start = int(input("Enter start number: "))
 print_num(start, num)
+
+
+# This one prints numbers from 1 to Stop.
+
+def print_num_stop(stop):
+    
+    if stop == 0:
+        return stop
+    print_num_stop(stop - 1)
+    print(stop)
+
+
+stop_1 = int(input("Enter stop number: "))
+print_num(stop_1)
 
