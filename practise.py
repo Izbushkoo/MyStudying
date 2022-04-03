0eb65940038a4f782f03bb66f3bc0a7598206db1
IzbushkO <98118884+Izbushkoo@users.noreply.github.com>
2022-04-03 13:18:25 +0300

    Update practise.py

--- practise.py
+++ practise.py
@@ -1,7 +1,9 @@
 import os
 
 def find_path(item):
+    
     # Knowing the nearest point we're searching for absolute path to file.
+    
     for root, dirs, files in os.walk(os.path.abspath(os.path.join('..', '..', 'Desktop'))):
         for name in files:
             if name == item:
@@ -10,21 +12,29 @@ def find_path(item):
 
 
 file = open(find_path('group_1.txt'), 'r')
+
 # make a list of the first group scores
+
 score_lst = []
 for i_line in file:
     score_lst.append(int(i_line.split()[2]))
+    
 # find the score difference
+
 difference = score_lst[0]
 for item in score_lst[1:]:
     difference -= item
 
 file_2 = open(find_path('group_2.txt'), 'r')
+
 # find the result of multiplying of the second group scores
+
 result = 1
 for i_line in file_2:
     result *= int(i_line.split()[2])
+    
 # print summ, difference and result
+
 print('Sum of scores of the first group:', sum(score_lst))
 print('Difference of scores of the first group:', difference)
 print('Compose of scores of the first group:', result)
