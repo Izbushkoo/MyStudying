332bd1a26c57f621a1ed306da2a9753e331c87c9
IzbushkO <98118884+Izbushkoo@users.noreply.github.com>
2022-04-04 20:35:27 +0300

    Update find_path.py

--- find_path.py
+++ find_path.py
@@ -1,7 +1,7 @@
 import os
 def find_path(file):
 
-    for root, dirs, files in os.walk(os.path.sep):
+    for root, dirs, files in os.walk(os.path.sep): # by means of "os.path.sep" it will work only with linux =)
         for name in files:
             if name == file:
                 path_to = os.path.abspath(os.path.join(root, name))
